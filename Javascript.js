886a0466026623a5ce1945c933fa62673d4908d9
gcodegaby <gaby.meneses1@gmail.com>
2024-03-21 19:24:53 -0400

    Yassa Home page

---
+++ Javascript.js
@@ -0,0 +1,26 @@
+let newsletterform = document.getElementById("newsletterform");
+
+newsletterform.addEventListener("submit", (e) => {
+e.preventDefault();
+let firstname = document.getElementById("first-name");
+let lastname = document.getElementById("last-name");
+let email = document.getElementById("email-address");
+let emailRegex = /\S+@\S+\.\S+/;
+
+if (firstname.value.length <= 2 ||lastname.value.length <= 2 ) {
+    document.getElementById("alertMessage").innerText = "Error: Please include more than 2 characters";
+    document.getElementById("customAlert").style.display = "block";
+}else if(!emailRegex.test(email.value)){
+    document.getElementById("alertMessage").innerText = "Error:Enter a valid email address";
+    document.getElementById("customAlert").style.display = "block";
+}else {
+    document.getElementById("alertMessage").innerText = "This form has been successfully submitted!";
+    document.getElementById("customAlert").style.display = "block";
+    console.log(
+        `This form has a name of ${firstname.value} ${lastname.value} and email of ${email.value}`
+    );
+    firstname.value = "";
+    lastname.value = "";
+    email.value = "";
+}
+});
