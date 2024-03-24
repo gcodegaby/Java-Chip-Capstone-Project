1b496053b64daf982f2d04bc5aeb6374fe280ca2
gcodegaby <gaby.meneses1@gmail.com>
2024-03-24 18:31:00 -0400

    Joshua updates

--- Newsletter Javascript.js
+++ Newsletter Javascript.js
@@ -1,19 +1,10 @@
-let newsletterform = document.getElementById("newsletterform");
-const mobileNavSection = document.getElementById("mobilenav");
-const hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
-//Mobile Nav Menu//
-const handleMobileNav = (e) => {
-mobileNavSection.classList.toggle("hidden");
-}
-hamburgerMenu.addEventListener("click", handleMobileNav)
-
+const newsletterform = document.getElementById("newsletterform");
 newsletterform.addEventListener("submit", (e) => {
 e.preventDefault();
 let firstname = document.getElementById("first-name");
 let lastname = document.getElementById("last-name");
 let email = document.getElementById("email-address");
 let emailRegex = /\S+@\S+\.\S+/;
-
 if (firstname.value.length <= 2 ||lastname.value.length <= 2 ) {
     document.getElementById("alertMessage").innerText = "Error: Please include more than 2 characters";
     document.getElementById("customAlert").style.display = "block";
@@ -31,3 +22,4 @@ if (firstname.value.length <= 2 ||lastname.value.length <= 2 ) {
     email.value = "";
 }
 });
+
